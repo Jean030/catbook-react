1db906b6578c7f4476b3992ddc7bf03cf3fda753
Jean030 <jean.rq.zhu@outlook.com>
2025-03-10 15:41:05 +0800

    w2s2

--- client/src/components/pages/Feed.jsx
+++ client/src/components/pages/Feed.jsx
@@ -17,10 +17,21 @@ const Feed = () => {
     //   creator_name: "creator name",
     //   content: "story content",
     // }
+    const story1 = { _id: "1", creator_name: "person1", content: "story1" };
+    const story2 = { _id: "2", creator_name: "person2", content: "story2" };
+    const story3 = { _id: "3", creator_name: "person3", content: "story3" };
+    const hardcodedStories = [story1, story2, story3];
+    setStories(hardcodedStories);
   }, []);
 
-  return <SingleStory _id="test_id" creator_name="Evan" content="test" />;
+  return (
+    <div>
+      <SingleStory _id="test_id" creator_name="test_creator_name" content="test_content" />
+      {JSON.stringify(stories)}
+    </div>
+  );
   // TODO (step2): render a SingleStory with hardcoded props
+
   // TODO (step3): map the state to SingleStory components
   // TODO (step4): add in the NewStory component and pass down addStory as a prop
   // TODO (step6): use Card instead of SingleStory, passing down the same props
