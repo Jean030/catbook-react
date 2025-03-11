8adf6b3125783d43b0970f83e3a9a281e90899cf
Jean030 <jean.rq.zhu@outlook.com>
2025-03-11 17:49:53 +0800

    s9 lost

--- client/src/components/modules/Card.jsx
+++ client/src/components/modules/Card.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { get } from "../../utilities";
 import SingleStory from "./SingleStory";
 import SingleComment from "./SingleComment";
-import { NewComment } from "./NewPostInput";
 import CommentsBlock from "./CommentsBlock";
 
 import "./Card.css";
@@ -17,33 +17,10 @@ import "./Card.css";
 const Card = (props) => {
   const [comments, setComments] = useState([]);
 
-  // updates the comments state so that the new comment is added immediately
-  const addNewComment = (comment) => {
-    setComments(comments.concat(comment));
-  };
-
   useEffect(() => {
-    const comment1 = {
-      _id: "commentid1",
-      creator_name: "person1",
-      parent: "id1",
-      content: "comment1",
-    };
-    const comment2 = {
-      _id: "commentid2",
-      creator_name: "person2",
-      parent: "id2",
-      content: "comment2",
-    };
-    const comment3 = {
-      _id: "commentid3",
-      creator_name: "person3",
-      parent: "id3",
-      content: "comment3",
-    };
-    const hardcodedComments = [comment1, comment2, comment3];
-
-    setComments(hardcodedComments.filter((comment) => comment.parent == props._id));
+    get("/api/comment", { parent: props._id }).then((commentItems) => {
+      setComments(commentItems);
+    });
   }, []);
 
   let commentsList = null;
@@ -63,7 +40,7 @@ const Card = (props) => {
   return (
     <div className="Card-container">
       <SingleStory _id={props._id} creator_name={props.creator_name} content={props.content} />
-      <CommentsBlock storyId={props._id} addNewComment={addNewComment} />
+      <CommentsBlock storyId={props._id} comments={comments} />
     </div>
   );
 };
